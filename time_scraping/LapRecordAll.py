9c72ee3645c1d1481b5db260d73a286d0ae9ae10
ishi <shingo.ishii@pwc.com>
2024-05-02 17:11:03 +0900

    スクレイピング先のデータの一部が空でもエラー停止しないように変更

--- time_scraping/LapRecordAll.py
+++ time_scraping/LapRecordAll.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium.webdriver.common.by import By
 from urllib.parse import quote
@@ -6,16 +8,19 @@ import pandas as pd
 import openpyxl
 from openpyxl.utils.dataframe import dataframe_to_rows 
 
+start_time = time.time()
+
 # オプション設定とブラウザの起動
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(options=chrome_options)
+wait_time=3
 
 # 指定URLにアクセス
 base_url = "http://racenow-motegi-1.racelive.jp/pages/live.html"
 driver.get(base_url)
 
-time.sleep(3)  # ページが完全に読み込まれるまで待機
+time.sleep(wait_time)  # ページが完全に読み込まれるまで待機
 
 # 個別ページのURLを取得
 monitor_list = driver.execute_script("return monitorList;")
@@ -35,34 +40,37 @@ workbook = openpyxl.Workbook()
 dfs = {}
 
 # 各選手のデータを取得してDataframeに格納
-for link in links:
+for link in links[:5]:
     driver.get(link)
-    time.sleep(3)
+    time.sleep(wait_time)
     element = driver.find_element(By.XPATH, '//*[@id="GapListArea"]/table')
     tdlist = element.find_elements(By.TAG_NAME, 'td')
 
     # 空白を削除
-    data = [elem.text for elem in tdlist if elem.text != '']
+    data = [elem.text for elem in tdlist]
 
-     # 最初のデータセグメントが不完全かどうかを確認
-    remainder = len(data) % 8
-    if remainder != 0 and len(data) > remainder:  # 最初のセグメントが不完全であれば
-        data = data[:remainder] + [''] * (8 - remainder) + data[remainder:]
+    f_data = []
+    for i in range(len(data), 0, -17):
+        segment = data[i-17:i]
+        # 最後の8個のデータを削除
+        f_data = segment[:-8] + f_data
 
-    if len(data) <= 2:  # データが2以下であればスキップ
+    if len(f_data) <= 1:
         continue
+    print(f_data)
 
     # データを分割してDataFrameに変換
     formatted_data = []
-    for i in range(0, len(data), 8):
-        segment = data[i:i+8]
+    for i in range(0, len(f_data), 9):
+        segment = f_data[i:i+9]
         # 不足分を空欄で補う
-        while len(segment) < 8:
+        while len(segment) < 9:
             segment.append('')
         formatted_data.append(segment)
 
-    df = pd.DataFrame(formatted_data, columns=['lap', 'name', 'sec1', 'sec2', 'sec3', 'speed', 'sec4', 'record'])
-    name = df['Name'].iloc[0]
+    df = pd.DataFrame(formatted_data, columns=['lap', 'name', 'sec1', 'sec2', 'sec3', 'speed', 'sec4', 'record','pit'])
+    print(df)
+    name = df['name'].iloc[0]
     if name not in dfs:
         dfs[name] = df
     else:
@@ -79,3 +87,9 @@ for name, df in dfs.items():
 
 workbook.save('gap.xlsx')
 driver.quit()
+
+end_time = time.time()
+# 処理にかかった時間(秒)を計算
+elapsed_time = end_time - start_time
+# 処理にかかった時間を表示
+print(f"処理にかかった時間: {elapsed_time:.2f}秒")
